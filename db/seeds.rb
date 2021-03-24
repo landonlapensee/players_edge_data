af0c0a32e6c14fa9110a52880e0dfd59d9892795
Landon Lapensee <landonlapensee@gmail.com>
2021-03-24 08:57:52 -0700

    Created Index and seed to display projection player names.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+## Parsing player projections provided by Blake S.
+require 'csv'
+
+Projection.destroy_all
+
+CSV.foreach(Rails.root.join('lib/seeds/projections.csv'), headers: true, header_converters: :symbol, encoding: 'ISO-8859-1') do |row|
+  data = row.to_hash
+  
+  Projection.create(
+    player: data[:player],
+    age: data[:age],
+    pos: data[:pos],
+    team: data[:team],
+    toi_org_es: data[:toi_org_es]
+  )
+end
+puts "Projections Created"
+
